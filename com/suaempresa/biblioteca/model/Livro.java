e21741233c142525aebfbd66ac7c342f0bd18705
Thiago Miguel <thiagomiguel045@gmail.com>
2025-05-28 21:43:23 -0300

    feat: Adiciona classe Livro e seus atributos/metodos

---
+++ com/suaempresa/biblioteca/model/Livro.java
@@ -0,0 +1,68 @@
+package com.suaempresa.biblioteca.model;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private boolean disponivel;
+
+    public Livro(int id, String titulo, String autor, String genero) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.disponivel = true;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                " | Título: " + titulo +
+                " | Autor: " + autor +
+                " | Gênero: " + genero +
+                " | Disponível: " + (disponivel ? "Sim" : "Não");
+    }
+}
